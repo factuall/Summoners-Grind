2fc17de82bc02377c17d4d25d9029b8922c3b806
Factual <63786641+factuall@users.noreply.github.com>
2021-08-27 09:40:39 +0200

    Added moving enemy
    code still randomly thrown into one file lol

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //canvas setup
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 1280;
-canvas.height = 720;
+canvas.width = 800;
+canvas.height = 600;
 ctx.font = '30px Arial'
 var objects = [];
 
@@ -29,10 +29,11 @@ function drawRect(x, y, w, h, c){
 
 let lastRender = Date.now();
 var intervalId = window.setInterval(function(){
-    update();
     //frame cap
     if(Date.now() > lastRender+15){
+        update();
         render();
+        
         lastRender = Date.now();
     }
 }, 3);
@@ -40,13 +41,81 @@ var intervalId = window.setInterval(function(){
 
 class Live{
     constructor(){
+        this.index = objects.length;
+        this.type = "Object";
         this.x = 0; 
         this.y = 0;
         this.w = 50;
         this.h = 50;
-        this.c = "#88ff88";
+        this.c = "#000000";
     }
 }
+
+class Player extends Live{
+    constructor(){
+        super();
+        this.type = "Player";
+        this.c = "#88ff88";
+        this.inPosition = true;
+        this.underAttack = true;
+    }
+    updatePlayer(){
+        this.inPosition = (collisionDetection(this, objects[1]));
+        if(!this.inPosition){
+            let distX = this.x - objects[1].x;
+            let distY = this.y - objects[1].y;
+            let destination = new Vector(distX, distY);
+            destination.normalize();
+            this.x -= destination.x * 4;
+            this.y -= destination.y * 4;
+        }
+    }
+};
+
+class Enemy extends Live{
+    constructor(){
+        super();
+        this.x = 400;
+        this.y = 400;
+        this.c = "#ff3333";
+        this.type = "Enemy";
+        this.enemyType = "Mage"
+        this.target = "None"
+    }
+    updateEnemy(){
+        switch(this.enemyType){
+            case "Mage":
+                if(this.target == "None"){
+                    let closest;
+                    let closestDist = 9999999;
+                    objects.forEach(element => {
+                        if(element.type == "Player" || element.type == "Ally"){
+                            let distX = this.x - element.x;
+                            let distY = this.y - element.y;
+                            let lengthXY = Math.sqrt(distX * distY);
+                            if(lengthXY < closestDist){
+                                closestDist = lengthXY;
+                                closest = element.index;
+                            }
+                        }
+                    });
+                    this.target = closest;
+                }else if(!collisionDetection(this,objects[this.target])){
+
+                    let distX = this.x - objects[this.target].x;
+                    let distY = this.y - objects[this.target].y;
+                    let destination = new Vector(distX, distY);
+                    destination.normalize();
+                    this.x -= destination.x * 1;
+                    this.y -= destination.y * 1;
+                
+                }
+                break;
+            default:
+                break;
+        }
+    }
+};
 
 function render(){
     drawRect(0,0,canvas.width,canvas.height, "#505050");
@@ -66,22 +135,19 @@ function collisionDetection(colA, colB){
 }
 
 
-class Player{
-    constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.w = 70;
-        this.h = 70;
-        this.c = "#88ff88";
-        this.inPosition = true;
-        this.underAttack = true;
-    }
-};
 
-var cursor = new Live();
-cursor.c = "#000000";
+
+
+
 objects.push(new Player());
+var cursor = new Live();
+cursor.c = "rgba(225,225,225,0.1)";
+
 objects.push(cursor);
+objects.push(new Enemy());
+var eeenemy = new Enemy();
+eeenemy.x = 600;
+objects.push(eeenemy);
 
 /////////////////////////////////////////
 //vector normalizing by Max Maximilian
@@ -96,18 +162,12 @@ this.x = this.x/length; //assigning new value to x (dividing x by length of the 
 this.y= this.y/length; //assigning new value to y
 }
 
-var deltaTime = Date.now();
 function update(){
-    deltaTime = Date.now() - deltaTime;
-    objects[0].inPosition = (collisionDetection(objects[0], objects[1]));
-    if(!objects[0].inPosition){
-        let distX = objects[0].x - objects[1].x;
-        let distY = objects[0].y - objects[1].y;
-        let destination = new Vector(distX, distY);
-        destination.normalize();
+    objects[0].updatePlayer();
+    objects.forEach(element => {
+        if(element.type == "Enemy"){
+            element.updateEnemy();
+        }
+    });
 
-        objects[0].x -= destination.x * 3;
-        objects[0].y -= destination.y * 3;
-    }
-    deltaTime = Date.now();
 }
