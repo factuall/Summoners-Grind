40b6f3ca85d2399d7997dbac3a868559bc3dd21c
Factual <adrianpolv@gmail.com>
2021-09-13 12:26:17 +0200

    Updated enemies and projectiles to use new moving functions

--- public/javascripts/objects.js
+++ public/javascripts/objects.js
@@ -77,10 +77,10 @@ class Player extends Life{
     }
 
     updatePlayer(){
-        this.inPosition = (CollisionDetection(this, objects[1]));
+        this.inPosition = (CollisionDetection(this, cursor));
         if(!this.inPosition){
-            let destination = GetFacingVector(this, objects[1]);
-            this.move(-destination.x*4,-destination.y*4);
+            let destination = GetFacingVectorCC(this, cursor);
+            this.move(-destination.x*4*deltaTime,-destination.y*4*deltaTime);
         }
         this.skills.forEach(playerSkill => {
             playerSkill.clock += deltaTime;
@@ -96,47 +96,21 @@ class Player extends Life{
         MPBar.style.width = (100 * (this.mana / this.maxMana)) - 1 + "%";
         
         //skills
-        //this is not that temporary... i guess...
         this.skills.forEach(function(playerSkill, i){
             SkillsGUI[i].style.backgroundColor = (playerSkill.clock >= playerSkill.cooldown) ? "gray" : "rgb(55,55,55)";
             let timeLeft = (playerSkill.cooldown - playerSkill.clock) / 60;
-            
-            //timeLeft = (Math.round(timeLeft * 10) / 10).toFixed(1);
             SkillsGUI[i].innerHTML = playerSkill.label + ((playerSkill.clock >= playerSkill.cooldown) ? "" : HTMLBEAK + timeLeft.toFixed(1) + "s");
         });
     }
 
     
 
-    inputKey(e){ //this is temporary too please dont kill me
+    inputKey(e){
         this.skills.forEach(playerSkill => {
             if(e.code == playerSkill.keycode && playerSkill.clock > playerSkill.cooldown){
                 playerSkill.clock = 0;
             }
         });
-        
-        switch(e.code){
-            case "KeyQ":
-
-                break;
-            case "KeyW":
-                if(this.CooldownClockW > this.CooldownW){
-                    this.CooldownClockW = 0;
-                }
-                break;
-            case "KeyE":
-                if(this.CooldownClockE > this.CooldownE){
-                    this.CooldownClockE = 0;
-                }
-                break;
-            case "KeyR":
-                if(this.CooldownClockR > this.CooldownR){
-                    this.CooldownClockR = 0;
-                }
-                break;
-            default:
-                break;
-        }
     }
 };
 
@@ -172,19 +146,18 @@ class Enemy extends Life{
 
         let collided = false;
         objects.forEach(element => {
-            if(CollisionDetection(this,element) && element!=this && objects[1] != element){
+            if(CollisionDetection(this,element) && element!=this && cursor != element){
                 if(element.name == "Player") collided = true;
                 if(element.name == "Enemy" && !collided){
                     let destination = GetFacingVector(this, element);
-                    this.x += destination.x * deltaTime;
-                    this.y += destination.y * deltaTime;
+                    this.move(destination.x*deltaTime,destination.y*deltaTime);
                     collided = true;
                 }
             }
 
         });
         if(!CollisionDetection(this,objects[this.target])){
-            if(GetDistanceBetweenObjects(this,objects[this.target]) < 100){
+            if(GetDistanceBetweenObjects(this,objects[this.target]) < 300){
                 if(this.lastAttack>(1000/this.attackSpeed)){
                     objects.push(new Projectile(this, objects[this.target], 4, this.attackDamage));
                     this.lastAttack = 0;
@@ -192,8 +165,7 @@ class Enemy extends Life{
 
             }else{
                 let destination = GetFacingVector(this, objects[this.target]);
-                this.x -= destination.x * 1;
-                this.y -= destination.y * 1;
+                this.move(-destination.x*deltaTime,-destination.y*deltaTime);
             }
 
         
@@ -218,20 +190,16 @@ class Projectile extends Life{
         this.damage = damage;
         this.w = 10;
         this.h = 10;
-        this.x = source.x + (source.w / 2) - (this.w / 2);
-        this.y = source.y + (source.h / 2) - (this.h / 2);
-
+        this.x = source.ox - (this.w / 2);
+        this.y = source.oy - (this.h / 2);
+        this.updateCetralPosition();
         this.c = "#ffff33";
         this.name = "Projectile";
     }
     
     updateProjectile(){
-        let targetWithOffset = new Life();
-        targetWithOffset.x = this.target.x + (this.target.w / 2) - (this.w / 2);
-        targetWithOffset.y = this.target.y + (this.target.h / 2) - (this.h / 2);
-        let destination = GetFacingVector(this, targetWithOffset);
-        this.x -= destination.x * deltaTime * this.speed;
-        this.y -= destination.y * deltaTime * this.speed;
+        let destination = GetFacingVectorCC(this, this.target);
+        this.move(-destination.x*deltaTime*this.speed, -destination.y*deltaTime*this.speed);
     
         if(CollisionDetection(this,this.target)){
             if(this.target.name == "Player") this.target.damagePlayer(this.damage);
