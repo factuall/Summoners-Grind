ab00d0ac8f8cdfb4de5976292e22a452c4043b7e
Factual <adrianpolv@gmail.com>
2021-09-16 12:38:16 +0200

    Animation class

--- public/javascripts/graphics.js
+++ public/javascripts/graphics.js
@@ -12,3 +12,44 @@ class Sprite{
         };
     }
 }
+
+class Animation{
+    constructor(frames, delay, loop){
+        this.currentFrame = 0;
+        this.frames = frames;
+        this.delay = delay;
+        this.loop = loop;
+        this.playing = false;
+        this.clock = 0;
+    }
+    getCurrentSprite(){
+        return this.frames[this.currentFrame];
+    }
+    playAnimation(){
+        this.playing = true;
+        this.currentFrame = 0;
+        this.clock = 0;
+    }
+    stopAnimation(){
+        this.playing = false;
+    }
+    toggleAnimation(){
+        this.playing != this.playing;
+    }
+    updateAnimation(deltaTime){
+        if(this.playing){
+            if(this.clock <= this.delay){
+                this.clock += deltaTime;
+            }else{
+                this.currentFrame++;
+                this.clock = 0;
+            }
+        }
+    }
+} 
+
+class AnimationController{
+    constructor(){
+
+    }
+}
