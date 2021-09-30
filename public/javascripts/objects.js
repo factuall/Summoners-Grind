be52be023b018ce588772992a1d83a95a5f15fb4
Factual <adrianpolv@gmail.com>
2021-09-30 12:15:38 +0200

    add CombatEntity

--- public/javascripts/objects.js
+++ public/javascripts/objects.js
@@ -53,6 +53,70 @@ class Trash extends Entity{
     }
 }
 
+class CombatEntity extends Entity{
+    constructor(){
+        super();
+        this.entityType = "CombatEntity";
+        //stats
+        this.maxHealth = 1200;
+        this.maxMana = 200;
+        this.health = this.maxHealth;
+        this.mana = this.maxMana;
+        this.moveSpeed = 4;
+        //combat
+        this.target = "None"
+        this.range = 200;
+        this.combatType = "range";
+        this.attackDamage = 20;
+        this.attackSpeed = 20;
+        this.lastAttack = 0;
+        //combat //important only if combat type is range
+        this.projectileSpeed = 4;
+    }
+
+    dealDamage(damage){
+        if(this.health >= damage)
+        this.health -= damage;
+
+    }
+
+    tryToAttack(){
+        if(this.lastAttack > (1000 / this.attackSpeed)){
+            if(this.combatType == "melee") {
+                this.lastAttack = 0;
+                objects[this.target].dealDamage(this.attackDamage);
+            }else{
+                this.lastAttack = 0;
+                objects.push(new Projectile(this, objects[this.target], this.projectileSpeed, this.attackDamage));
+            }
+        }
+    }
+
+    combatTarget(){
+        let collided = false;
+        objects.forEach(element => {
+            if(CollisionDetection(this,element) && element!=this && cursor != element){
+                if(element.name == this.target.name) collided = true;
+                if(element.entityType == "CombatEntity" && !collided){
+                    let destination = GetFacingVector(this, element);
+                    this.move(destination.x*deltaTime,destination.y*deltaTime);
+                }
+            }
+
+        });
+        if(!CollisionDetection(this,objects[this.target])){
+            if(GetDistanceBetweenObjects(this,objects[this.target]) < this.range && this.combatType == "range"){
+                this.tryToAttack();
+            }else{
+                let destination = GetFacingVector(this, objects[this.target]);
+                this.move(-destination.x*this.moveSpeed*deltaTime,-destination.y*this.moveSpeed*deltaTime);
+            }
+        }else if(this.combatType == "melee"){
+            this.tryToAttack();
+        }
+    }
+}
+
 class Player extends Entity{
     constructor(){
         super();
@@ -213,7 +277,7 @@ class Enemy extends Entity{
                     if(element.name == "Enemy" && !collided){
                         let destination = GetFacingVector(this, element);
                         this.move(destination.x*deltaTime,destination.y*deltaTime);
-                        collided = true;
+                        //collided = true;
                     }
                 }
     
