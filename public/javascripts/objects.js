56eec49c9779c0f3a6e3e96e4d9172eca6160a7d
Factual <adrianpolv@gmail.com>
2021-10-11 15:37:39 +0200

    render healthbar with text

--- public/javascripts/objects.js
+++ public/javascripts/objects.js
@@ -303,16 +303,23 @@ class CombatEntityInfo extends Entity{
         this.healthBar.w = 60;
         this.healthBar.h = 6;
         this.healthBar.drawContent = "#ff0000";
+        this.healthText = new Entity();
+        this.healthText.drawContent = new TextSprite("100%", "monospace", 15, "#ffffff");
     }
     updateObject(){
         let healthPercentage = this.owner.health / this.owner.maxHealth;
         this.setCentralPosition(this.owner.ox, this.owner.y - 10);
         this.healthBar.setCentralPosition(this.x + (this.healthBar.w / 2) + 2, this.oy);
         this.healthBar.w = 60 * healthPercentage;
+        this.healthText.setPosition(this.ox - ((this.healthText.drawContent.text.toString().length * this.healthText.drawContent.size) / 4), this.y + 9);
+        this.healthText.drawContent.text = this.owner.health;
         super.updateObject();
     }
     renderObject(){
-        super.renderObject();
-        this.healthBar.renderObject();
+        if(this.owner.health < this.owner.maxHealth){
+            super.renderObject();
+            this.healthBar.renderObject();
+            this.healthText.renderObject();   
+        }
     }
 }
